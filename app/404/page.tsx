d955964596018f96c83d23bb602a7387ed2b69e8
gdnpro <genderson2637@gmail.com>
2025-11-01 14:34:57 -0400

    404 fixed

---
+++ app/404/page.tsx
@@ -0,0 +1,8 @@
+export default function Page404() {
+  return (
+    <div className="flex justify-center items-center bg-white">
+      <h2>Page not found</h2>
+      <h1>404</h1>
+    </div>
+  )
+}
