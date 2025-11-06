1f7ba0525316201f23ec18037459b261f85414a4
RikiRilis <thewhitzip@gmail.com>
2025-11-06 01:18:40 -0400

    Refactor layout structure: Migrate layout logic from ClientLayoutWrapper to RootLayout, integrating header, footer, live chat, and scroll-to-top functionalities directly. Enhance code organization and simplify layout management with conditional rendering based on route paths.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,52 @@
+"use client"
+
 import "./globals.css"
 import "./butterup.css"
+import LiveChat from "@/components/ui/LiveChat"
+import Footer from "@/components/ui/Footer"
+import Header from "@/components/ui/Header"
+import "@/libs/toast"
+import { usePathname } from "next/navigation"
+import { AuthProvider } from "@/components/ui/AuthContext"
+import { useEffect } from "react"
+import { butterup } from "@/libs/toast"
 import SEO from "@/components/SEO"
+import { GoToTop } from "@/components/ui/GoToTop"
 import { Analytics } from "@vercel/analytics/next"
-import ClientLayoutWrapper from "@/components/ClientLayoutWrapper"
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const noLayoutRoutes = ["/auth/login", "/auth/register"]
+  const hideLayout = noLayoutRoutes.includes(usePathname())
+
+  useEffect(() => {
+    window.toast = butterup.toast.bind(butterup)
+  }, [])
+
   return (
-    <html lang="es">
+    <html data-scroll-behavior="smooth" lang="es">
       <head>
         <SEO
           title="GDN Pro"
-          description="GDN Pro es una plataforma..."
+          description="GDN Pro es una plataforma en la que desarrolladores freelancers y clientes en búsqueda de trabajadores se encuentran y llegan a acuerdos corporativos. En GDN PRO creamos soluciones digitales innovadoras. Desarrollo web, apps móviles y marketing digital de clase mundial."
           canonical="https://gdnpro.com"
         />
       </head>
-
       <body>
         <Analytics />
-        <ClientLayoutWrapper>{children}</ClientLayoutWrapper>
+
+        <AuthProvider>
+          {!hideLayout && <Header />}
+
+          <main>{children}</main>
+
+          {!hideLayout && <GoToTop />}
+          {!hideLayout && <LiveChat />}
+          {!hideLayout && <Footer />}
+        </AuthProvider>
       </body>
     </html>
   )
